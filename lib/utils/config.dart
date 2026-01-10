e4b346723068af957a1e62ef9ca4ac9abc171ec3
IrtazaDev <irtazakhalidll@gmail.com>
2026-01-10 20:50:18 +0500

    minor change

---
+++ lib/utils/config.dart
@@ -0,0 +1,6 @@
+class Config {
+  static const String agoraAppId = 'c4a1309f72be434592965a29b64c1fd4';
+
+  // You can add more configuration constants here
+  // e.g., API base URLs, feature flags, etc.
+}
