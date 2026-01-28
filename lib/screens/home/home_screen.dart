f8ac6a24fe03f75cd05668639336b2a437d346f0
IrtazaDev <irtazakhalidll@gmail.com>
2026-01-28 11:51:54 +0500

    fix

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -20,7 +20,7 @@ import 'package:curio_campus/screens/home/widgets/more_options_sheet.dart';
 import 'package:curio_campus/utils/logger.dart';
 
 class HomeScreen extends StatefulWidget {
-  const HomeScreen({Key? key}) : super(key: key);
+  const HomeScreen({super.key});
 
   @override
   State<HomeScreen> createState() => _HomeScreenState();
