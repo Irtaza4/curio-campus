4c84e9e9ef2d60abc1f75cef65900f86ed4b4e42
IrtazaDev <irtazakhalidll@gmail.com>
2026-01-28 11:51:30 +0500

    fix

--- lib/screens/auth/login_screen.dart
+++ lib/screens/auth/login_screen.dart
@@ -8,7 +8,7 @@ import 'package:curio_campus/widgets/custom_button.dart';
 import 'package:curio_campus/widgets/custom_text_field.dart';
 
 class LoginScreen extends StatefulWidget {
-  const LoginScreen({Key? key}) : super(key: key);
+  const LoginScreen({super.key});
 
   @override
   State<LoginScreen> createState() => _LoginScreenState();
