feef93940d2a1795321f43223544e3404d0ac135
IrtazaDev <irtazakhalidll@gmail.com>
2026-02-27 14:54:19 +0500

    fix

--- lib/utils/config.dart
+++ lib/utils/config.dart
@@ -1,5 +1,7 @@
 class Config {
   static const String agoraAppId = 'c4a1309f72be434592965a29b64c1fd4';
+  static const String appName = 'Curio Campus';
+  static const String supportEmail = 'support@curiocampus.com';
 
   // You can add more configuration constants here
   // e.g., API base URLs, feature flags, etc.
