830ad49d1f030e0a32aa76b5147cec7dffdc96d9
IrtazaDev <irtazakhalidll@gmail.com>
2026-03-21 09:53:25 +0500

    fix

--- lib/screens/auth/register_screen.dart
+++ lib/screens/auth/register_screen.dart
@@ -122,7 +122,7 @@ class _RegisterScreenState extends State<RegisterScreen> {
       body: SafeArea(
         child: SingleChildScrollView(
           child: Padding(
-            padding: const EdgeInsets.all(24.0),
+            padding: const EdgeInsets.all(AppTheme.defaultPadding * 1.5),
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
