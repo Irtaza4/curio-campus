0835e63f2e67dc9cdfeb8618a7ca40e748f4a104
IrtazaDev <irtazakhalidll@gmail.com>
2026-05-04 13:39:19 +0500

    fixes

--- lib/screens/project/create_project_screen.dart
+++ lib/screens/project/create_project_screen.dart
@@ -635,7 +635,7 @@ class _CreateProjectScreenState extends State<CreateProjectScreen> {
                             shape: RoundedRectangleBorder(
                               borderRadius: BorderRadius.circular(AppTheme.defaultBorderRadius / 2),
                             ),
-                            deleteIcon: const Icon(Icons.close, size: 18),
+                            deleteIcon: const Icon(Icons.close_rounded, size: 18),
                             onDeleted: () {
                               setState(() {
                                 _selectedTeamMembers.remove(userId);
