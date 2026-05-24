faf1ed9eaece1f4fc409d3c6954c091f0b5bc26f
IrtazaDev <irtazakhalidll@gmail.com>
2026-05-24 12:49:30 +0500

    fixed

--- lib/utils/app_theme.dart
+++ lib/utils/app_theme.dart
@@ -181,6 +181,8 @@ class AppTheme {
     ),
     outlinedButtonTheme: OutlinedButtonThemeData(
       style: secondaryButtonStyle,
+
+      
     ),
     textButtonTheme: TextButtonThemeData(
       style: TextButton.styleFrom(
