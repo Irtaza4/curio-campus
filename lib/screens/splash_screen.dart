49fc2849ca78b70036da8fd649b5722047af9875
IrtazaDev <irtazakhalidll@gmail.com>
2026-05-28 14:53:03 +0500

    fix

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -20,6 +20,7 @@ class SplashScreen extends StatefulWidget {
   State<SplashScreen> createState() => _SplashScreenState();
 }
 
+
 class _SplashScreenState extends State<SplashScreen>
     with SingleTickerProviderStateMixin {
   late AnimationController _controller;
