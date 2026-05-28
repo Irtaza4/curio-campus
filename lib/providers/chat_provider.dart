bcb2cc07a610e7bb6b671fceece0f83b9b9be27a
IrtazaDev <irtazakhalidll@gmail.com>
2026-05-28 14:52:45 +0500

    fix

--- lib/providers/chat_provider.dart
+++ lib/providers/chat_provider.dart
@@ -20,6 +20,7 @@ class ChatProvider with ChangeNotifier {
   String? _errorMessage;
   String? _currentChatId;
 
+
   // For real-time updates
   StreamSubscription<QuerySnapshot>? _messagesSubscription;
   StreamSubscription<QuerySnapshot>? _chatsSubscription;
