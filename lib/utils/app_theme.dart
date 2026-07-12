2c3fe06da9a739dd2d0da461bc3a9b7758f975c3
IrtazaDev <irtazakhalidll@gmail.com>
2026-07-12 13:39:58 +0500

    fix

--- lib/utils/app_theme.dart
+++ lib/utils/app_theme.dart
@@ -108,8 +108,6 @@ class AppTheme {
     ),
   );
 
-
-
   // Button styles - Dark Theme
   static final ButtonStyle darkPrimaryButtonStyle = ElevatedButton.styleFrom(
     backgroundColor: darkPrimaryColor,
